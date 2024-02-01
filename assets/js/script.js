5f6d34ee05c54788e6b09825acd378908716d313
GTodd711 <150283557+GTodd711@users.noreply.github.com>
2024-01-31 21:11:08 -0800

    tried fixing JS pulling work from main branch

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,28 +2,32 @@ var sign = document.getElementById('sign');
 var day = document.getElementById('day');
 var submitBtn = document.getElementById('submitBtn');
 
+console.log("event listener attached");
 
 submitBtn.addEventListener("click", function(event) {
-    event.preventDefault();
+  event.preventDefault();
   
-    var zodiacSign = sign.value;
-    var selectedDay = day.value;
+  var zodiacSign = sign.value;
   
-    var apiUrl = `https://aztro.sameerkumar.website/?sign=${zodiacSign}&day=${selectedDay}`;
+  var apiUrl = `/${zodiacSign}`;
   
-    fetch(apiUrl)
-      .then(function(response) {
-        return response.json();
-      })
-      .then(function(data) {
-        console.log( "response body:", data);
-        var horoscope = data.description;
-        document.getElementById("artHoro").textContent = horoscope;
-        document.getElementById("artHoro").style.display = "block";
-      })
-      .catch(function(error) {
-        console.log(error);
-      });
-    });
-
-
+  fetch(apiUrl)
+  .then(function(response) {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.text(); // Handle response as text
+  })
+  .then(function(data) {
+    try {
+      var horoscope = JSON.parse(data).description;
+      document.getElementById("artHoro").textContent = horoscope;
+      document.getElementById("artHoro").style.display = "block";
+    } catch (error) {
+      console.log('Error parsing JSON:', error);
+    }
+  })
+  .catch(function(error) {
+    console.log(error);
+  });
+});
