c425cf964e08ae997c5345ae5834d9d33f91f8c2
GTodd711 <150283557+GTodd711@users.noreply.github.com>
2024-01-31 19:20:34 -0800

    added js and touched up html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,29 @@
-var astro = document.querySelector('.astro');
-var tarot = document.querySelector('.tarot');
-var advice = document.querySelector('.advice');
-var nameBtn = document.querySelector('.nameBtn');
-var userText = document.querySelector('.userName');
-var birthBtn = document.querySelector('.birthBtn');
-var userBirth = document.querySelector('.userBirth');
+var sign = document.getElementById('sign');
+var day = document.getElementById('day');
+var submitBtn = document.getElementById('submitBtn');
+
+
+submitBtn.addEventListener("click", function(event) {
+    event.preventDefault();
+  
+    var zodiacSign = sign.value;
+    var selectedDay = day.value;
+  
+    var apiUrl = `https://aztro.sameerkumar.website/?sign=${zodiacSign}&day=${selectedDay}`;
+  
+    fetch(apiUrl)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(data) {
+        console.log( "response body:", data);
+        var horoscope = data.description;
+        document.getElementById("artHoro").textContent = horoscope;
+        document.getElementById("artHoro").style.display = "block";
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+    });
+
+
