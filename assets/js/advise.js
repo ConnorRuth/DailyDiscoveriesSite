87482939091f373895bdf29d21c923cefe608d8d
ConnorRuth <152222829+ConnorRuth@users.noreply.github.com>
2024-02-01 21:28:24 -0800

    added copy button to advice.html

--- assets/js/advise.js
+++ assets/js/advise.js
@@ -1,7 +1,7 @@
 // Declare our variables
 const resDiv = document.querySelector('#results');
 const resBtn = document.querySelector('#getData');
-
+const copyBtn = document.getElementById('clipboardBtn')
 
 resBtn.addEventListener('click', () => {
   getAdvice();
@@ -24,4 +24,13 @@ function getAdvice() {
     console.log(error);
   });
 
+}
+copyBtn.addEventListener('click', () => {
+  copyText();
+});
+
+function copyText() {
+  let copyText = document.getElementById("results").textContent;
+  window.getSelection(copyText);
+  navigator.clipboard.writeText(copyText);
 }
