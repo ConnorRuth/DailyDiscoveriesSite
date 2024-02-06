ba1e3f9c97d5e68e843a99d1a619fec5e4a2d1cd
ConnorRuth <152222829+ConnorRuth@users.noreply.github.com>
2024-02-05 21:39:18 -0800

    added function to display card imgs

--- assets/js/tarot.js
+++ assets/js/tarot.js
@@ -10,14 +10,14 @@ var TarotImg = [{shortname: "pe10", tarotImage: "./assets/imgs/Tarot/TenofPentac
 {shortname: "waqu", tarotImage: "./assets/imgs/Tarot/QueenofWands.jpg"},{shortname: "wakn", tarotImage: "./assets/imgs/Tarot/KnightofWands.jpg"},{shortname: "sw10", tarotImage: "./assets/imgs/Tarot/TenofSwords.jpg"},{shortname: "sw09", tarotImage: "./assets/imgs/Tarot/NineofSwords.jpg"}, {shortname: "sw08", tarotImage: "./assets/imgs/Tarot/EightofSwords.jpg"},
 {shortname: "sw07", tarotImage: "./assets/imgs/Tarot/SevenofSwords.jpg"},{shortname: "sw06", tarotImage: "./assets/imgs/Tarot/SixofSwords.jpg"},{shortname: "sw05", tarotImage: "./assets/imgs/Tarot/FiveofSwords.jpg"},{shortname: "sw04", tarotImage: "./assets/imgs/Tarot/FourofSwords.jpg"},
 {shortname: "sw03", tarotImage: "./assets/imgs/Tarot/ThreeofSwords.jpg"},{shortname: "sw02", tarotImage: "./assets/imgs/Tarot/TwoofSwords.jpg"},{shortname: "swac", tarotImage: "./assets/imgs/Tarot/AceofSwords.jpg"},{shortname: "swki", tarotImage: "./assets/imgs/Tarot/KingofSwords.jpg"}, {shortname: "swpa", tarotImage: "./assets/imgs/Tarot/PageofSwords.jpg"},
-{shortname: "swqu", tarotImage: "./assets/imgs/Tarot/QueenofSwords.jpg"},{shortname: "swkn", tarotImage: "./assets/imgs/Tarot/KnightofSwords.jpg"},,,,,,,,,,{shortname: "cu10", tarotImage: "./assets/imgs/Tarot/TenofCups.jpg"},{shortname: "cu09", tarotImage: "./assets/imgs/Tarot/NineofCups.jpg"}, {shortname: "cu08", tarotImage: "./assets/imgs/Tarot/EightofCups.jpg"},
+{shortname: "swqu", tarotImage: "./assets/imgs/Tarot/QueenofSwords.jpg"},{shortname: "swkn", tarotImage: "./assets/imgs/Tarot/KnightofSwords.jpg"},{shortname: "cu10", tarotImage: "./assets/imgs/Tarot/TenofCups.jpg"},{shortname: "cu09", tarotImage: "./assets/imgs/Tarot/NineofCups.jpg"}, {shortname: "cu08", tarotImage: "./assets/imgs/Tarot/EightofCups.jpg"},
 {shortname: "cu07", tarotImage: "./assets/imgs/Tarot/SevenofCups.jpg"},{shortname: "cu06", tarotImage: "./assets/imgs/Tarot/SixofCups.jpg"},{shortname: "cu05", tarotImage: "./assets/imgs/Tarot/FiveofCups.jpg"},{shortname: "cu04", tarotImage: "./assets/imgs/Tarot/FourofCups.jpg"},
 {shortname: "cu03", tarotImage: "./assets/imgs/Tarot/ThreeofCups.jpg"},{shortname: "cu02", tarotImage: "./assets/imgs/Tarot/TwoofCups.jpg"},{shortname: "cuac", tarotImage: "./assets/imgs/Tarot/AceofCups.jpg"},{shortname: "cuki", tarotImage: "./assets/imgs/Tarot/KingofCups.jpg"}, {shortname: "cupa", tarotImage: "./assets/imgs/Tarot/PageofCups.jpg"},
 {shortname: "cuqu", tarotImage: "./assets/imgs/Tarot/QueenofCups.jpg"},{shortname: "cukn", tarotImage: "./assets/imgs/Tarot/KnightofCups.jpg"},{shortname: "ar14", tarotImage: "./assets/imgs/Tarot/Temperance.jpg"},{shortname: "ar18", tarotImage: "./assets/imgs/Tarot/TheMoon.jpg"},{shortname: "ar01", tarotImage: "./assets/imgs/Tarot/TheMagician.jpg"},
 {shortname: "ar12", tarotImage: "./assets/imgs/Tarot/TheHangedMan.jpg"},{shortname: "ar09", tarotImage: "./assets/imgs/Tarot/TheHermit.jpg"},{shortname: "ar05", tarotImage: "./assets/imgs/Tarot/TheHeirophant.jpg"},{shortname: "ar07", tarotImage: "./assets/imgs/Tarot/TheChariot.jpg"},{shortname: "ar00", tarotImage: "./assets/imgs/Tarot/TheFool.jpg"},
 {shortname: "ar11", tarotImage: "./assets/imgs/Tarot/Justice.jpg"},{shortname: "ar15", tarotImage: "./assets/imgs/Tarot/TheDevil.jpg"},{shortname: "ar10", tarotImage: "./assets/imgs/Tarot/TheWheelofFortune.jpg"},{shortname: "ar04", tarotImage: "./assets/imgs/Tarot/TheEmperor.jpg"},{shortname: "ar02", tarotImage: "./assets/imgs/Tarot/TheHighPriestess.jpg"},
 {shortname: "ar03", tarotImage: "./assets/imgs/Tarot/TheEmpress.jpg"},{shortname: "ar19", tarotImage: "./assets/imgs/Tarot/TheSun.jpg"},{shortname: "ar20", tarotImage: "./assets/imgs/Tarot/Judgement.jpg"},{shortname: "ar13", tarotImage: "./assets/imgs/Tarot/Death.jpg"},{shortname: "ar21", tarotImage: "./assets/imgs/Tarot/TheWorld.jpg"},
-{shortname: "ar17", tarotImage: "./assets/imgs/Tarot/TheStar.jpg"},{shortname: "ar06", tarotImage: "./assets/imgs/Tarot/TheLovers.jpg"},{shortname: "ar08", tarotImage: "./assets/imgs/Tarot/Strength.jpg"},{shortname: "ar16", tarotImage: "./assets/imgs/Tarot/TheTower.jpg"},];
+{shortname: "ar17", tarotImage: "./assets/imgs/Tarot/TheStar.jpg"},{shortname: "ar06", tarotImage: "./assets/imgs/Tarot/TheLovers.jpg"},{shortname: "ar08", tarotImage: "./assets/imgs/Tarot/Strength.jpg"},{shortname: "ar16", tarotImage: "./assets/imgs/Tarot/TheTower.jpg"}];
 function drawCard(event) {
     event.preventDefault();
     var reading = document.getElementById("reading").value;
@@ -28,16 +28,16 @@ function drawCard(event) {
             return response.json();
         } else throw new Error('Failed to get cards. error: ' + response.status);
     }).then(function (data) {
-        console.log(data);
+      console.log(data);
         let htmlCard =""
         for(let i=0;i<data.cards.length;i++){
-          console.log(data.cards[i].name);
+          var tarotDisplay = matchTarot(data.cards[i]);
             htmlCard += `
             <div class="flip-card">
                 <h3>${data.cards[i].name}</h3>
-                <h6>${data.cards[i].type}</h6>
             <div class="flip-card-inner">
               <div class="flip-card-front">
+              <img src="${tarotDisplay.tarotImage}">
                           </div>
               <div class="flip-card-back">
                 <h4>Meaning Upright:</h4>
@@ -55,5 +55,8 @@ function drawCard(event) {
             console.warn(error.msg);
         })
 }
-
+function matchTarot(data){
+  const selectedTarot = TarotImg.find(ele => ele.shortname === data.name_short);
+   return selectedTarot;
+}
 drawBtn.addEventListener("click", drawCard);
